7266b1f997d22cfb14e41d9e647fcb74fbf0775f
zmmbreeze02 <zmmbreeze@gmail.com>
2024-12-03 22:51:54 +0800

    [feature] update

--- src/fft_recursion.rs
+++ src/fft_recursion.rs
@@ -27,7 +27,7 @@ pub fn fft_recursion(samples: &[f64]) -> Result<Vec<Complex<f64>>, FFTError> {
     let mut result = vec![Complex::ZERO; len];
     let mut cache = HashMap::new();
     for k in 0..len {
-        result[k] = _fft_recursion_k(k, samples.to_vec(), &mut cache);
+        result[k] = _fft_recursion_k(len, k, samples.to_vec(), &mut cache);
     }
 
     Ok(result)
@@ -35,31 +35,37 @@ pub fn fft_recursion(samples: &[f64]) -> Result<Vec<Complex<f64>>, FFTError> {
 
 // result = W_N^k = e^{-j * 2\pi * k / N}
 fn _gen_w_n(k: usize, len: usize) -> Complex<f64> {
+    if k == 0 || k == len {
+        return Complex::new(1.0, 0.0);
+    }
+    if len == 2 * k {
+        return Complex::new(-1.0, 0.0);
+    }
+    if len == 4 * k {
+        return Complex::new(0.0, -1.0);
+    }
+    if len * 3 == 4 * k {
+        return Complex::new(0.0, 1.0);
+    }
     let len_f64 = len as f64;
     let k_f64 = k as f64;
     let theta = -2.0 * PI * k_f64 / len_f64;
     Complex::new(theta.cos(), theta.sin())
 }
 
-fn _fft_recursion_k(k: usize, samples: Vec<f64>, cache: &mut HashMap<(usize, usize), (Complex<f64>, Complex<f64>)>) -> Complex<f64> {
+fn _fft_recursion_k(position: usize, k: usize, samples: Vec<f64>, cache: &mut HashMap<(usize, usize, usize), (Complex<f64>, Complex<f64>)>) -> Complex<f64> {
     let len = samples.len();
     if len == 1 {
         // X[k] = x[0] * W_N^{0*k} = x[0]
-        // if k > 1 {
-        //     println!("k={}, len={}, v={}", k, len, samples[0]);
-        // }
         return Complex::new(samples[0], 0.0);
     }
 
     if k >= len/2 {
-        // X[k] = X_{odd}[k] - W_N^k * X_{even}[k]
+        // X[k] = X_{even}[k] - W_N^k * X_{odd}[k]
         // use cached X_{odd}[k] and W_N^k * X_{even}[k]
         let real_k = k - len/2;
-        if let Some(cached) = cache.get(&(len, real_k)) {
-            // if k <= 1 {
-                println!("cached k={}, len={}, l={} - r={}", k, len, cached.0, cached.1);
-            // }
-            return cached.0 - cached.1;
+        if let Some((l, r)) = cache.get(&(len, real_k, position)) {
+            return l - r;
         }
         unreachable!();
     }
@@ -73,12 +79,10 @@ fn _fft_recursion_k(k: usize, samples: Vec<f64>, cache: &mut HashMap<(usize, usi
     let odd_samples: Vec<f64> = odd_indices.into_iter().map(|(_, value)| *value).collect();
     let even_samples: Vec<f64> = even_indices.into_iter().map(|(_, value)| *value).collect();
     // combine odd and even part
-    let l = _fft_recursion_k(k, even_samples, cache);
-    let r = _gen_w_n(k, len) * _fft_recursion_k(k, odd_samples, cache);
-    cache.insert((len, k), (l, r));
-    // if k > 1 {
-    //     println!("k={}, len={}, l={} + r={}", k, len, l, r);
-    // }
+    let l = _fft_recursion_k(position, k, even_samples, cache);
+    let r = _gen_w_n(k, len) * _fft_recursion_k(position | (len/2), k, odd_samples, cache);
+
+    cache.insert((len, k, position), (l, r));
     l + r
 }
 
@@ -101,35 +105,35 @@ mod tests {
 
     #[test]
     fn test_calc_spectrum_by_fft_recursion() -> Result<(), FFTError> {
-        let spectrum = calc_spectrum_by_fft_recursion(&mock_sine(vec![5.0], vec![0.0], 1, 64.0), 64.0)?;
-        println!("xxxxx: {:?}", spectrum.clone().into_iter().map(|v| (v.0, v.1.norm())).collect::<Vec<(f64, f64)>>());
-        let r = find_frequency_in_spectrum(spectrum, Some(8.0));
-        println!("{:?}", r);
+        let sine = mock_sine(vec![5.0], vec![0.0], 2, 1024.0);
+        let spectrum = calc_spectrum_by_fft_recursion(&sine, 1024.0)?;
+        let r = find_frequency_in_spectrum(spectrum, None);
+        // println!("\n{:?}", r);
         assert_eq!(r.len(), 1);
         assert_eq!(r[0].0, 5.0);
 
-        // let spectrum = calc_spectrum_by_fft_recursion(&mock_sine(vec![5.0, 10.0], vec![0.0, 10.0], 2, 1024.0), 1024.0)?;
-        // let r = find_frequency_in_spectrum(spectrum, None);
-        // // println!("{:?}", r);
-        // assert_eq!(r.len(), 2);
-        // assert_eq!(r[0].0, 5.0);
-        // assert_eq!(r[1].0, 10.0);
+        let spectrum = calc_spectrum_by_fft_recursion(&mock_sine(vec![5.0, 10.0], vec![0.0, 10.0], 2, 1024.0), 1024.0)?;
+        let r = find_frequency_in_spectrum(spectrum, None);
+        // println!("{:?}", r);
+        assert_eq!(r.len(), 2);
+        assert_eq!(r[0].0, 5.0);
+        assert_eq!(r[1].0, 10.0);
 
-        // let spectrum = calc_spectrum_by_fft_recursion(&mock_sine(vec![5.0, 10.0, 7000.0], vec![0.0, 10.0, 10000.0], 2, 16.0*1024.0), 16.0*1024.0)?;
-        // let r = find_frequency_in_spectrum(spectrum, None);
-        // // println!("{:?}", r);
-        // assert_eq!(r.len(), 3);
-        // assert_eq!(r[0].0, 5.0);
-        // assert_eq!(r[1].0, 10.0);
-        // assert_eq!(r[2].0, 7000.0);
+        let spectrum = calc_spectrum_by_fft_recursion(&mock_sine(vec![5.0, 10.0, 7000.0], vec![0.0, 10.0, 10000.0], 2, 16.0*1024.0), 16.0*1024.0)?;
+        let r = find_frequency_in_spectrum(spectrum, None);
+        // println!("{:?}", r);
+        assert_eq!(r.len(), 3);
+        assert_eq!(r[0].0, 5.0);
+        assert_eq!(r[1].0, 10.0);
+        assert_eq!(r[2].0, 7000.0);
 
-        // let spectrum = calc_spectrum_by_fft_recursion(&mock_cosine(vec![5.0, 10.0, 7000.0], vec![0.0, 10.0, 10000.0], 2, 16.0*1024.0), 16.0*1024.0)?;
-        // let r = find_frequency_in_spectrum(spectrum, None);
-        // // println!("{:?}", r);
-        // assert_eq!(r.len(), 3);
-        // assert_eq!(r[0].0, 5.0);
-        // assert_eq!(r[1].0, 10.0);
-        // assert_eq!(r[2].0, 7000.0);
+        let spectrum = calc_spectrum_by_fft_recursion(&mock_cosine(vec![5.0, 10.0, 7000.0], vec![0.0, 10.0, 10000.0], 2, 16.0*1024.0), 16.0*1024.0)?;
+        let r = find_frequency_in_spectrum(spectrum, None);
+        // println!("{:?}", r);
+        assert_eq!(r.len(), 3);
+        assert_eq!(r[0].0, 5.0);
+        assert_eq!(r[1].0, 10.0);
+        assert_eq!(r[2].0, 7000.0);
 
         Ok(())
     }
