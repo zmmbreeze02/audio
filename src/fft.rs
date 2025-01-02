d90dc2a112b70685e8e5eb440e1d29d2dfb6dc2f
zmmbreeze02 <zmmbreeze@gmail.com>
2025-01-02 18:00:36 +0800

    [feature]fft

--- src/fft.rs
+++ src/fft.rs
@@ -1,9 +1,6 @@
 use num_complex::Complex;
-use plotters::prelude::IntoLinspace;
-use rand::seq::index;
 use thiserror::Error;
 use std::f64::consts::PI;
-use std::collections::HashMap;
 
 
 
@@ -17,7 +14,7 @@ pub enum FFTError {
     Unknown,
 }
 
-pub fn fft(samples: &mut [f64]) -> Result<Vec<Complex<f64>>, FFTError> {
+pub fn fft(samples: &[f64]) -> Result<Vec<Complex<f64>>, FFTError> {
     let len = samples.len();
     if len <= 1 {
         return Err(FFTError::NotEnoughSamples(len));
@@ -26,16 +23,13 @@ pub fn fft(samples: &mut [f64]) -> Result<Vec<Complex<f64>>, FFTError> {
         return Err(FFTError::NotPowerOfTwo(len));
     }
 
-    // bit reverse
-    _bit_reverse(samples);
+    // Turn into Complex Vector
+    let mut samples: Vec<Complex<f64>> = samples.into_iter().map(|s| Complex::new(*s, 0.0)).collect();
 
-    let mut result = vec![Complex::ZERO; len];
-    let mut cache = HashMap::new();
-    for k in 0..len {
-        result[k] = _fft_recursion_k(len, k, samples.to_vec(), &mut cache);
-    }
+    _bit_reverse(&mut samples);
+    _butterflies(&mut samples);
 
-    Ok(result)
+    Ok(samples)
 }
 
 /**
@@ -61,7 +55,7 @@ pub fn fft(samples: &mut [f64]) -> Result<Vec<Complex<f64>>, FFTError> {
  * 2. output = 0000
  * 3. output = 0100
  */
-fn _bit_reverse(samples: &mut [f64]) {
+fn _bit_reverse(samples: &mut Vec<Complex<f64>>) {
     let len = samples.len();
     let max_index = len - 1;
     let mut reserved_index = 0;
@@ -106,24 +100,27 @@ fn _bit_reverse(samples: &mut [f64]) {
 /**
  * Use butterflies calculation to calc the FFT result.
  */
-fn _butterflies(samples: &mut [f64]) -> Complex<f64> {
-    let len = samples.len();
+fn _butterflies(samples: &mut Vec<Complex<f64>>) {
+    let n = samples.len();
+    let max_stage = n.ilog2();
 
-    for l in (0..len).step(2) {
-        
-    }
-
-    for (int len = 2; len <= N; len <<= 1) {
-        double ang = -2 * PI / len;
-        complex w = {cos(ang), sin(ang)};
-        for (int i = 0; i < N; i += len) {
-            complex w_pow = {1, 0};
-            for (int j = 0; j < len / 2; j++) {
-                complex u = a[i + j];
-                complex t = complexMul(w_pow, a[i + j + len / 2]);
-                a[i + j] = {u.real + t.real, u.imag + t.imag};
-                a[i + j + len / 2] = {u.real - t.real, u.imag - t.imag};
-                w_pow = complexMul(w_pow, w);
+    for stage in 1..=max_stage {
+        // Length of each block
+        let l = 2usize.pow(stage);
+        // Split samples into blocks
+        // for example when n=8, the blocks are:
+        // stage=1,l=2 => 0,1|2,3|4,5|6,7
+        // stage=2,l=4 => 0,1,2,3|4,5,6,7
+        // stage=3,l=8 => 0,1,2,3,4,5,6,7
+        for block_head_index in (0..n).step_by(l) {
+            // half of L
+            let half_l = l/2;
+            // iterate through each parts
+            let block_tail_index = block_head_index + half_l;
+            for i in block_head_index..block_tail_index {
+                let twiddle = _calc_twiddle(i % half_l, l);
+                samples[i] = samples[i] + samples[i + half_l] * twiddle;
+                samples[i + half_l] = samples[i] - samples[i + half_l] * twiddle;
             }
         }
     }
@@ -150,42 +147,8 @@ fn _calc_twiddle(k: usize, len: usize) -> Complex<f64> {
     Complex::new(theta.cos(), theta.sin())
 }
 
-fn _fft_recursion_k(position: usize, k: usize, samples: Vec<f64>, cache: &mut HashMap<(usize, usize, usize), (Complex<f64>, Complex<f64>)>) -> Complex<f64> {
-    let len = samples.len();
-    if len == 1 {
-        // X[k] = x[0] * W_N^{0*k} = x[0]
-        return Complex::new(samples[0], 0.0);
-    }
-
-    if k >= len/2 {
-        // X[k] = X_{even}[k] - W_N^k * X_{odd}[k]
-        // use cached X_{odd}[k] and W_N^k * X_{even}[k]
-        if let Some((l, r)) = cache.get(&(len, k - len/2, position)) {
-            return l - r;
-        }
-        unreachable!();
-    }
-
-    // println!("k:{}, len:{}, pos:{}, samples:{:?}", k, len, position, samples);
-
-    // X[k] = X_{even}[k] + W_N^k * X_{odd}[k]
-    // split samples into odd and even part
-    let (even_indices, odd_indices): (Vec<_>, Vec<_>) = samples
-        .iter()
-        .enumerate()
-        .partition(|&(index, _)| index % 2 == 0);
-    let odd_samples: Vec<f64> = odd_indices.into_iter().map(|(_, value)| *value).collect();
-    let even_samples: Vec<f64> = even_indices.into_iter().map(|(_, value)| *value).collect();
-    // combine odd and even part
-    let l = _fft_recursion_k(position, k, even_samples, cache);
-    let r = _calc_twiddle(k, len) * _fft_recursion_k(position | (len/2), k, odd_samples, cache);
-
-    cache.insert((len, k, position), (l, r));
-    l + r
-}
-
 // calc spectrum as `Vec<(frequency, complex_result)>`
-pub fn calc_spectrum_by_fft(samples: &mut [f64], sample_rate: f64) -> Result<Vec<(f64, Complex<f64>)>, FFTError> {
+pub fn calc_spectrum_by_fft(samples: &[f64], sample_rate: f64) -> Result<Vec<(f64, Complex<f64>)>, FFTError> {
     let sample_count = samples.len() as f64;
     let spectrum = fft(samples)?
         .into_iter()
@@ -198,45 +161,36 @@ pub fn calc_spectrum_by_fft(samples: &mut [f64], sample_rate: f64) -> Result<Vec
 
 #[cfg(test)]
 mod tests {
+    use num_complex::Complex;
     use super::{calc_spectrum_by_fft, FFTError, _bit_reverse};
     use super::super::mock::{mock_sine, mock_cosine, find_frequency_in_spectrum};
 
 
     #[test]
     fn test_bit_reverse() {
-        let mut samples: Vec<f64> = (0..4).map(|v| v as f64).collect();
+        let mut samples:Vec<Complex<f64>> = (0..4).map(|v| Complex::new(v as f64, 0.0)).collect();
         _bit_reverse(&mut samples);
-        // samples.iter().enumerate().for_each(|(i, v)| println!("{} = {} = {:02b}", i, v.clone() as usize,v.clone() as usize));
-        assert_eq!(samples, vec![0.0, 2.0, 1.0, 3.0]);
+        let result:Vec<f64> = samples.into_iter().map(|v| v.re).collect();
+        assert_eq!(result, vec![0.0, 2.0, 1.0, 3.0]);
 
-        let mut samples: Vec<f64> = (0..8).map(|v| v as f64).collect();
+        let mut samples:Vec<Complex<f64>> = (0..8).map(|v| Complex::new(v as f64, 0.0)).collect();
         _bit_reverse(&mut samples);
-        // samples.iter().enumerate().for_each(|(i, v)| println!("{} = {} = {:03b}", i, v.clone() as usize,v.clone() as usize));
-        assert_eq!(samples, vec![0.0, 4.0, 2.0, 6.0, 1.0, 5.0, 3.0, 7.0]);
+        let result:Vec<f64> = samples.into_iter().map(|v| v.re).collect();
+        assert_eq!(result, vec![0.0, 4.0, 2.0, 6.0, 1.0, 5.0, 3.0, 7.0]);
 
-        let mut samples: Vec<f64> = (0..16).map(|v| v as f64).collect();
+        let mut samples:Vec<Complex<f64>> = (0..16).map(|v| Complex::new(v as f64, 0.0)).collect();
         _bit_reverse(&mut samples);
-        // samples.iter().enumerate().for_each(|(i, v)| println!("{} = {} = {:04b}", i, v.clone() as usize,v.clone() as usize));
-        assert_eq!(samples, vec![0.0, 8.0, 4.0, 12.0, 2.0, 10.0, 6.0, 14.0, 1.0, 9.0, 5.0, 13.0, 3.0, 11.0, 7.0, 15.0]);
+        let result:Vec<f64> = samples.into_iter().map(|v| v.re).collect();
+        assert_eq!(result, vec![0.0, 8.0, 4.0, 12.0, 2.0, 10.0, 6.0, 14.0, 1.0, 9.0, 5.0, 13.0, 3.0, 11.0, 7.0, 15.0]);
     }
 
     #[test]
     fn test_calc_spectrum_by_fft() -> Result<(), FFTError> {
-        // let sine = mock_sine(vec![1.0], vec![0.0], 1, 8.0);
-        // println!("original: {:?}", sine);
-        // let spectrum = calc_spectrum_by_fft(&sine, 8.0)?;
-        // let sp: Vec<(f64,f64)> = spectrum.clone().into_iter().map(|(k, v)| (k, v.norm())).collect();
-        // println!("result: {:?}", sp);
-        // let r = find_frequency_in_spectrum(spectrum, None);
-        // // println!("\n{:?}", r);
-        // assert_eq!(r.len(), 1);
-        // assert_eq!(r[0].0, 1.0);
-
-        // let spectrum = calc_spectrum_by_fft(&mock_sine(vec![5.0], vec![0.0], 2, 1024.0), 1024.0)?;
-        // let r = find_frequency_in_spectrum(spectrum, None);
-        // // println!("\n{:?}", r);
-        // assert_eq!(r.len(), 1);
-        // assert_eq!(r[0].0, 5.0);
+        let spectrum = calc_spectrum_by_fft(&mock_sine(vec![5.0], vec![0.0], 2, 1024.0), 1024.0)?;
+        let r = find_frequency_in_spectrum(spectrum, None);
+        println!("\n{:?}", r);
+        assert_eq!(r.len(), 1);
+        assert_eq!(r[0].0, 5.0);
 
         // let spectrum = calc_spectrum_by_fft(&mock_sine(vec![5.0, 10.0], vec![0.0, 10.0], 2, 1024.0), 1024.0)?;
         // let r = find_frequency_in_spectrum(spectrum, None);
